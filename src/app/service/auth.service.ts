8098d69ca8569bba46420ee5fea5836364610625
unknown <felipelauton021@gmail.com>
2021-05-06 15:24:18 -0300

    Implementação do put e delete

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,9 @@
+import { environment } from './../../environments/environment.prod';
+import { UserLogin } from './../model/UserLogin';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
-import { UserLogin } from '../model/UserLogin';
 
 @Injectable({
   providedIn: 'root'
@@ -19,16 +19,17 @@ export class AuthService {
   }
 
   cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar',user)
+    return this.http.post<User>('http://localhost:8080/usuarios/cadastrar', user)
   }
 
   logado(){
     let ok: boolean = false
 
-    if(environment.token != ''){
+    if (environment.token != ''){
       ok = true
     }
 
     return ok
   }
+  
 }
